b3e4a50162449d502eb31fd7b5844532faf6ed5c
veronica-elinor <66841183+veronica-elinor@users.noreply.github.com>
2021-07-02 12:33:06 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
         <!-- Change to "Écouter les étoiles" -->
         <Title>Sois belle à ta façon</Title>
     </head>
-    <header><h1>Sois belle à ta façon</h1><h2>Veronica's Page</h2><p>Welcome to my website.</p></header>
+    <header<img src=''</header>
     <body>
-        
+       <h1>Sois belle à ta façon</h1><h2>Veronica's Page<br>Welcome to my website.</h2>
 
     </body>
     <footer>By Ann Chang in 2021</footer>
