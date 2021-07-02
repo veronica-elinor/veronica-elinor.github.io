f18fb1c189155090f021773f55fc61b83ee9fcd8
veronica-elinor <66841183+veronica-elinor@users.noreply.github.com>
2021-07-02 12:10:17 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <Title>Sois Belle A Ta Facon</Title>
+    </head>
+    <header><h1>Sois Belle A Ta Facon</h1><h2>Veronica's Page</h2><p>Welcome to my website.</p></header>
+    <body>
+        
+
+    </body>
+    <footer>By Ann Chang in 2021</footer>
+</html>
